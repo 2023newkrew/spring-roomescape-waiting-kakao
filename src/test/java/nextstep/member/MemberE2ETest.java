88b22b30b9e6d760eeb6e682428cbaca70d1e1e9
Siryeong <siryeongchoi@gmail.com>
2023-01-16 16:29:51 +0900

    test: 토큰을 통해 내 정보를 조회한다.

--- src/test/java/nextstep/member/MemberE2ETest.java
+++ src/test/java/nextstep/member/MemberE2ETest.java
@@ -1,9 +1,11 @@
-package nextstep.auth;
+package nextstep.member;
 
 import io.restassured.RestAssured;
-import nextstep.member.MemberRequest;
-import nextstep.theme.ThemeRequest;
-import org.junit.jupiter.api.BeforeEach;
+import io.restassured.http.Header;
+import nextstep.auth.TokenRequest;
+import nextstep.auth.TokenResponse;
+
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,18 +13,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.core.Is.is;
 
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-public class AuthE2ETest {
-    public static final String USERNAME = "username";
-    public static final String PASSWORD = "password";
-    private Long memberId;
-
-    @BeforeEach
-    void setUp() {
-        MemberRequest body = new MemberRequest(USERNAME, PASSWORD, "name", "010-1234-5678");
+public class MemberE2ETest {
+    @DisplayName("멤버를 생성한다")
+    @Test
+    public void create() {
+        MemberRequest body = new MemberRequest("username", "password", "name", "010-1234-5678");
         RestAssured
                 .given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -32,86 +31,80 @@ public class AuthE2ETest {
                 .statusCode(HttpStatus.CREATED.value());
     }
 
-    @DisplayName("토큰을 생성한다")
+    @DisplayName("토큰을 통해 내 정보를 조회한다.")
     @Test
-    public void create() {
-        TokenRequest body = new TokenRequest(USERNAME, PASSWORD);
-        var response = RestAssured
-                .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(body)
-                .when().post("/login/token")
-                .then().log().all()
-                .statusCode(HttpStatus.OK.value())
-                .extract();
-
-        assertThat(response.as(TokenResponse.class)).isNotNull();
-    }
-
-    @DisplayName("아이디가 잘못된 토큰을 요청하는 경우")
-    @Test
-    public void invalidUsername() {
-        TokenRequest body = new TokenRequest(USERNAME+ "s", PASSWORD);
+    void me() {
+        // given
+        final String username = "davi-kane";
+        final String password = "password";
         RestAssured
                 .given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(body)
-                .when().post("/login/token")
-                .then().log().all()
-                .statusCode(HttpStatus.NOT_FOUND.value());
-    }
-    @DisplayName("비밀번호가 잘못된 토큰을 요청하는 경우")
-    @Test
-    public void invalidPassword() {
-        TokenRequest body = new TokenRequest(USERNAME, PASSWORD+ "s");
+                .body(new MemberRequest(username, password, "name", "010-1234-5678"))
+                .post("/members");
+
+        var response = RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(new TokenRequest(username, password))
+                .post("/login/token");
+
+        TokenResponse tokenResponse = response.as(TokenResponse.class);
+
+        // when
         RestAssured
                 .given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(body)
-                .when().post("/login/token")
-                .then().log().all()
-                .statusCode(HttpStatus.NOT_FOUND.value());
-    }
+                .header(new Header("Authorization", "Bearer " + tokenResponse.getAccessToken()))
+                .when().get("/members/me")
 
-    @DisplayName("테마 목록을 조회한다")
-    @Test
-    public void showThemes() {
-        createTheme();
-
-        var response = RestAssured
-                .given().log().all()
-                .param("date", "2022-08-11")
-                .when().get("/themes")
+        // then
                 .then().log().all()
                 .statusCode(HttpStatus.OK.value())
-                .extract();
-        assertThat(response.jsonPath().getList(".").size()).isEqualTo(1);
+                .body("username", is(username))
+                .body("password", is(password));
     }
 
-    @DisplayName("테마를 삭제한다")
-    @Test
-    void delete() {
-        Long id = createTheme();
-
-        var response = RestAssured
-                .given().log().all()
-                .when().delete("/themes/" + id)
-                .then().log().all()
-                .extract();
-
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
-    }
-
-    public Long createTheme() {
-        ThemeRequest body = new ThemeRequest("테마이름", "테마설명", 22000);
-        String location = RestAssured
-                .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(body)
-                .when().post("/themes")
-                .then().log().all()
-                .statusCode(HttpStatus.CREATED.value())
-                .extract().header("Location");
-        return Long.parseLong(location.split("/")[2]);
-    }
+//
+//    @DisplayName("테마 목록을 조회한다")
+//    @Test
+//    public void showThemes() {
+//        createTheme();
+//
+//        var response = RestAssured
+//                .given().log().all()
+//                .param("date", "2022-08-11")
+//                .when().get("/themes")
+//                .then().log().all()
+//                .statusCode(HttpStatus.OK.value())
+//                .extract();
+//        assertThat(response.jsonPath().getList(".").size()).isEqualTo(1);
+//    }
+//
+//    @DisplayName("테마를 삭제한다")
+//    @Test
+//    void delete() {
+//        Long id = createTheme();
+//
+//        var response = RestAssured
+//                .given().log().all()
+//                .when().delete("/themes/" + id)
+//                .then().log().all()
+//                .extract();
+//
+//        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+//    }
+//
+//    public Long createTheme() {
+//        ThemeRequest body = new ThemeRequest("테마이름", "테마설명", 22000);
+//        String location = RestAssured
+//                .given().log().all()
+//                .contentType(MediaType.APPLICATION_JSON_VALUE)
+//                .body(body)
+//                .when().post("/themes")
+//                .then().log().all()
+//                .statusCode(HttpStatus.CREATED.value())
+//                .extract().header("Location");
+//        return Long.parseLong(location.split("/")[2]);
+//    }
 }
