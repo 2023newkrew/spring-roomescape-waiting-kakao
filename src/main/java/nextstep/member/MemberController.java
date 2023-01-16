438cb37df082c73120701e20d738b1c9893787c4
Siryeong <SiryeongChoi@gmail.com>
2023-01-16 16:41:24 +0900

    feat: 인증토큰이 잘못된 경우 예외처리

--- src/main/java/nextstep/member/MemberController.java
+++ src/main/java/nextstep/member/MemberController.java
@@ -1,5 +1,6 @@
 package nextstep.member;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,9 +23,14 @@ public class MemberController {
 
     @GetMapping("/me")
     public ResponseEntity me(@RequestHeader("Authorization") String authorizationHeader) {
-        Long id = memberService.getPrincipal(authorizationHeader);
-        Member member = memberService.findById(id);
-        return ResponseEntity.ok(member);
+        try{
+            Long id = memberService.getPrincipal(authorizationHeader);
+            Member member = memberService.findById(id);
+            return ResponseEntity.ok(member);
+        }
+        catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
     }
 
 }
