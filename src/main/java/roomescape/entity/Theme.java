5c076ab04e01fc7b02961fa3efcbf92c1bf7c8b0
kane.woz <kane.woz@kakaocorp.com>
2023-01-18 13:17:33 +0900

    format: 코드 포매팅 재설정

--- src/main/java/roomescape/entity/Theme.java
+++ src/main/java/roomescape/entity/Theme.java
@@ -14,7 +14,7 @@ import javax.validation.constraints.PositiveOrZero;
 public class Theme {
     @NotNull
     private Long id;
-    
+
     @NotBlank
     private String name;
 
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -14,23 +14,23 @@ CREATE TABLE member
 
 create table theme
 (
-    id          bigint          not null    auto_increment      ,
-    name        varchar(20)     not null                        ,
-    desc        varchar(255)    not null                        ,
-    price       int             not null                        ,
+    id    bigint       not null auto_increment,
+    name  varchar(20)  not null,
+    desc  varchar(255) not null,
+    price int          not null,
     primary key (id),
     unique (name)
 );
 create table reservation
 (
-    id          bigint          not null    auto_increment      ,
-    date        date            not null                        ,
-    time        time            not null                        ,
-    name        varchar(20)     not null                        ,
-    theme_id    bigint          not null                        ,
-    member_id   bigint          not null                        ,
+    id        bigint      not null auto_increment,
+    date      date        not null,
+    time      time        not null,
+    name      varchar(20) not null,
+    theme_id  bigint      not null,
+    member_id bigint      not null,
     primary key (id),
     unique (date, time),
-    foreign key (theme_id) references theme(id),
-    foreign key (member_id) references member(id)
+    foreign key (theme_id) references theme (id),
+    foreign key (member_id) references member (id)
 );
