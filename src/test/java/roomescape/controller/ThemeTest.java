7bbc869e24dc5321cdc985a3f9f666132538e45c
kane.woz <kane.woz@kakaocorp.com>
2023-01-25 18:22:39 +0900

    test: 모든 테스트를 MockMVC + 기존 데이터베이스를 사용하는 형태로 변경

--- src/test/java/roomescape/controller/ThemeTest.java
+++ src/test/java/roomescape/controller/ThemeTest.java
@@ -1,116 +1,101 @@
 package roomescape.controller;
 
-import io.restassured.RestAssured;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 import roomescape.SpringWebApplication;
 import roomescape.dto.LoginControllerTokenPostBody;
+import roomescape.dto.LoginControllerTokenPostResponse;
 import roomescape.dto.ThemeControllerPostBody;
-import roomescape.entity.Theme;
 
 import java.util.Random;
 import java.util.UUID;
 
-import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @DisplayName("테마에 관련된 RESTful API 요청")
 @SpringBootTest(classes = {SpringWebApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 @ActiveProfiles({"web"})
 public class ThemeTest {
-    @Value("${local.server.port}")
-    int port;
+    private final ObjectMapper mapper = new ObjectMapper()
+            .registerModule(new JavaTimeModule());
+    @Autowired
+    private MockMvc mvc;
+
     private String adminToken;
 
     @BeforeEach
-    void setUp() {
-        RestAssured.port = port;
-        adminToken = RestAssured.given()
-                                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                                .body(new LoginControllerTokenPostBody("admin", "1q2w3e4r!"))
-                                .post("/api/login/token")
-                                .body().jsonPath().getString("access_token");
+    void setUp() throws Exception {
+        adminToken = mapper.readValue(
+                mvc.perform(post("/api/login/token")
+                           .contentType(MediaType.APPLICATION_JSON_VALUE)
+                           .content(mapper.writeValueAsString(new LoginControllerTokenPostBody("admin", "1q2w3e4r!"))))
+                   .andReturn()
+                   .getResponse()
+                   .getContentAsString(),
+                LoginControllerTokenPostResponse.class
+        ).getAccessToken();
     }
 
     @DisplayName("테마 생성")
+    @Transactional
     @Test
-    public void createTheme() {
-        RestAssured
-                .given()
-                .header("Authorization", "Bearer " + adminToken)
-                .contentType("application/json")
-                .body(new ThemeControllerPostBody(
-                        UUID.randomUUID().toString().split("-")[0],
-                        UUID.randomUUID().toString(),
-                        new Random().nextInt(0, 1000000)
-                ))
-                .when()
-                .post("/api/themes")
-                .then().log().all()
-                .statusCode(HttpStatus.CREATED.value());
+    public void createTheme() throws Exception {
+        mvc.perform(post("/api/themes")
+                   .header("Authorization", "Bearer " + adminToken)
+                   .contentType(MediaType.APPLICATION_JSON)
+                   .content(mapper.writeValueAsString(new ThemeControllerPostBody(
+                           UUID.randomUUID().toString().split("-")[0],
+                           UUID.randomUUID().toString(),
+                           new Random().nextInt(0, 1000000)
+                   )))
+           )
+           .andExpect(status().isCreated());
     }
 
     @DisplayName("테마 검색")
     @Test
-    public void getTheme() {
-        var expectedResult = new ThemeControllerPostBody(
-                UUID.randomUUID().toString().split("-")[0],
-                UUID.randomUUID().toString(),
-                new Random().nextInt(0, 1000000)
-        );
-        var postResult = RestAssured
-                .given()
-                .header("Authorization", "Bearer " + adminToken)
-                .contentType("application/json")
-                .body(expectedResult)
-                .post("/api/themes");
-        postResult.jsonPath().getList("exlist", Theme.class);
-        RestAssured
-                .when()
-                .get(postResult.header("Location"))
-                .then().log().all()
-                .statusCode(HttpStatus.OK.value())
-                .body("id", is(postResult.jsonPath().getInt("id")))
-                .body("name", is(expectedResult.getName()))
-                .body("desc", is(expectedResult.getDesc()))
-                .body("price", is(expectedResult.getPrice()));
+    public void getTheme() throws Exception {
+        mvc.perform(get("/api/themes/1"))
+           .andExpect(status().isOk())
+           .andExpect(jsonPath("$.id").value(1L))
+           .andExpect(jsonPath("$.name").value("기본테마"))
+           .andExpect(jsonPath("$.desc").value("테마설명"))
+           .andExpect(jsonPath("$.price").value(1234000))
+        ;
     }
 
     @DisplayName("테마 삭제")
+    @Transactional
     @Test
-    public void getThemes() {
-        var expectedResult = new ThemeControllerPostBody(
-                UUID.randomUUID().toString().split("-")[0],
-                UUID.randomUUID().toString(),
-                new Random().nextInt(0, 1000000)
-        );
-        var postResult = RestAssured
-                .given()
-                .header("Authorization", "Bearer " + adminToken)
-                .contentType("application/json")
-                .body(expectedResult)
-                .post("/api/themes");
+    public void deleteTheme() throws Exception {
+        mvc.perform(delete("/api/themes/2")
+                   .header("Authorization", "Bearer " + adminToken)
+           )
+           .andExpect(status().isNoContent());
+        mvc.perform(get("/api/themes/2"))
+           .andExpect(status().isNotFound());
+    }
 
-        RestAssured
-                .when()
-                .get(postResult.header("Location"))
-                .then().log().all()
-                .statusCode(HttpStatus.OK.value());
-        RestAssured
-                .given()
-                .header("Authorization", "Bearer " + adminToken)
-                .delete(postResult.header("Location"))
-                .then().log().all()
-                .statusCode(HttpStatus.NO_CONTENT.value());
-        RestAssured
-                .when()
-                .get(postResult.header("Location"))
-                .then().log().all()
-                .statusCode(HttpStatus.NOT_FOUND.value());
+    @DisplayName("사용중인 테마 삭제")
+    @Transactional
+    @Test
+    public void deleteUsingTheme() throws Exception {
+        mvc.perform(delete("/api/themes/1")
+                   .header("Authorization", "Bearer " + adminToken)
+           )
+           .andExpect(status().isConflict());
     }
 }
