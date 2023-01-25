809efbcfd8788fb36422caf1524ab9ea698d2b48
kane.woz <kane.woz@kakaocorp.com>
2023-01-25 18:23:05 +0900

    fix: json 프로퍼티 포매팅 방식 변경

--- src/main/java/roomescape/dto/MemberControllerGetResponse.java
+++ src/main/java/roomescape/dto/MemberControllerGetResponse.java
@@ -1,5 +1,6 @@
 package roomescape.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,6 +22,7 @@ public class MemberControllerGetResponse {
     private String name;
     @NotBlank
     private String phone;
+    @JsonProperty("is_admin")
     @NotNull
     private Boolean isAdmin;
 }
