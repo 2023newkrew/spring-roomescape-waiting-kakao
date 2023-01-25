e4734f5bf1ac46194c890a250648b93ff3f33751
엄태호 <qqsscc7342@gmail.com>
2023-01-25 18:00:37 +0900

    test: 예약 대기 테스트 추가

--- src/main/java/nextstep/domain/dto/WaitingResponse.java
+++ src/main/java/nextstep/domain/dto/WaitingResponse.java
@@ -1,23 +1,20 @@
 package nextstep.domain.dto;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 import nextstep.domain.persist.Schedule;
 import nextstep.domain.persist.Waiting;
 
-import java.util.Objects;
-
 @Getter
 @AllArgsConstructor
-@RequiredArgsConstructor
+@NoArgsConstructor
 public class WaitingResponse {
     private Long id;
     private Schedule schedule;
     private Long waitNum;
 
-    public WaitingResponse(Waiting waiting, long waitNum) {
+    public WaitingResponse(Waiting waiting, Long waitNum) {
         this.id = waiting.getId();
         this.schedule = waiting.getSchedule();
         this.waitNum = waitNum;
