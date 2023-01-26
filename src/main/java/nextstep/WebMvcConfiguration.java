41b9f34b7c91990b160c19ef1c883bda7d5442d0
HQkim <hyunkyu001@gmail.com>
2023-01-26 14:43:27 +0900

    refactor: auth 패키지를 nextstep 패키지로부터 분리

--- src/main/java/nextstep/WebMvcConfiguration.java
+++ src/main/java/nextstep/WebMvcConfiguration.java
@@ -1,9 +1,9 @@
 package nextstep;
 
-import nextstep.auth.AdminInterceptor;
-import nextstep.auth.JwtTokenProvider;
-import nextstep.auth.LoginMemberArgumentResolver;
-import nextstep.auth.LoginService;
+import auth.*;
+import nextstep.member.Member;
+import nextstep.member.MemberDao;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,21 +12,54 @@ import java.util.List;
 
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
-    private LoginService loginService;
-    private JwtTokenProvider jwtTokenProvider;
 
-    public WebMvcConfiguration(LoginService loginService, JwtTokenProvider jwtTokenProvider) {
-        this.loginService = loginService;
-        this.jwtTokenProvider = jwtTokenProvider;
+    private final MemberDao memberDao;
+
+    public WebMvcConfiguration(MemberDao memberDao){
+        this.memberDao = memberDao;
+    }
+
+    @Bean
+    public JwtTokenProvider jwtTokenProvider(){
+        return new JwtTokenProvider();
+    }
+
+    @Bean
+    public LoginService loginService(){
+        return new LoginService(
+                (username, password)->{
+                    Member member = memberDao.findByUsername(username);
+                    if(member.checkWrongPassword(password)){
+                        return null;
+                    }
+                    return new LoginService.TokenMember(
+                            member.getId(),
+                            member.getRole()
+                    );
+                },
+                (id)->{
+                    Member member = memberDao.findById(id);
+                    return new LoginService.TokenMember(
+                            member.getId(),
+                            member.getRole()
+                    );
+                },
+                jwtTokenProvider()
+        );
+    }
+
+    @Bean
+    public LoginController loginController(){
+        return new LoginController(loginService());
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new AdminInterceptor(jwtTokenProvider)).addPathPatterns("/admin/**");
+        registry.addInterceptor(new AdminInterceptor(jwtTokenProvider())).addPathPatterns("/admin/**");
     }
 
     @Override
     public void addArgumentResolvers(List argumentResolvers) {
-        argumentResolvers.add(new LoginMemberArgumentResolver(loginService));
+        argumentResolvers.add(new LoginMemberArgumentResolver(loginService()));
     }
 }
