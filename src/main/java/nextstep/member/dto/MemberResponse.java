1e17d1724b4c8f6ffa86622a1b3b40197ef78074
pluto.yun <pluto.yun@kakaocorp.com>
2023-01-26 15:33:46 +0900

    refactor: UserDetailsPrincipal 분리

--- src/main/java/nextstep/member/dto/MemberResponse.java
+++ src/main/java/nextstep/member/dto/MemberResponse.java
@@ -1,24 +1,25 @@
 package nextstep.member.dto;
 
 import auth.domain.UserRole;
-import auth.dto.UserDetailsResponse;
 import lombok.AccessLevel;
-import lombok.Getter;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
-
+@RequiredArgsConstructor
+@Data
 @Setter(AccessLevel.NONE)
-public class MemberResponse extends UserDetailsResponse {
+public class MemberResponse {
 
-    @Getter
+    private final Long id;
+
+    private final String username;
+
+    private final String password;
+
     private final String name;
 
-    @Getter
     private final String phone;
 
-    public MemberResponse(Long id, String username, String password, UserRole role, String name, String phone) {
-        super(id, username, password, role);
-        this.name = name;
-        this.phone = phone;
-    }
+    private final UserRole role;
 }
