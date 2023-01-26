2c04d93470222cf2233df8505d4b5b630a76a6f3
kyne.k <dd2307@snu.ac.kr>
2023-01-26 18:11:32 +0900

    feat : 나의 예약 목록 조회 구현

--- src/main/java/nextstep/waiting/WaitingController.java
+++ src/main/java/nextstep/waiting/WaitingController.java
@@ -3,11 +3,14 @@ package nextstep.waiting;
 import auth.LoginMember;
 import auth.UserDetails;
 import java.net.URI;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import nextstep.member.Member;
 import nextstep.member.MemberService;
+import nextstep.reservation.ReservationResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,12 +31,12 @@ public class WaitingController {
         return ResponseEntity.created(URI.create("/reservation-waitings/" + id)).build();
     }
 
-//    @GetMapping("/reservations")
-//    public ResponseEntity readReservations(@RequestParam Long themeId, @RequestParam String date) {
-//        List<Waiting> results = waitingService.findAllByThemeIdAndDate(themeId, date);
-//        return ResponseEntity.ok().body(results);
-//    }
-//
+    @GetMapping("/reservations-waitings/mine")
+    public ResponseEntity readReservations(@LoginMember UserDetails userDetails) {
+        List<WaitingResponse> results = waitingService.findAllByMemberId(userDetails.getId());
+        return ResponseEntity.ok().body(results);
+    }
+
     @DeleteMapping("/reservation-waitings/{id}")
     public ResponseEntity deleteReservation(@LoginMember UserDetails userDetails, @PathVariable Long id) {
         Member member = memberService.findById(userDetails.getId());
@@ -41,14 +44,4 @@ public class WaitingController {
 
         return ResponseEntity.noContent().build();
     }
-//
-//    @ExceptionHandler(Exception.class)
-//    public ResponseEntity onException(Exception e) {
-//        return ResponseEntity.badRequest().build();
-//    }
-//
-//    @ExceptionHandler(AuthenticationException.class)
-//    public ResponseEntity onAuthenticationException(AuthenticationException e) {
-//        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-//    }
 }
