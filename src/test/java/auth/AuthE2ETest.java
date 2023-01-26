b1b341de1c1b27f764482838277b02cac46c2d8e
정핸모 <gusah009@naver.com>
2023-01-26 16:21:26 +0900

    refactor: auth 패키지 이동
    - java/nextstep/auth -> java/auth

--- src/test/java/auth/AuthE2ETest.java
+++ src/test/java/auth/AuthE2ETest.java
@@ -1,5 +1,7 @@
-package nextstep.auth;
+package auth;
 
+import auth.TokenRequest;
+import auth.TokenResponse;
 import io.restassured.RestAssured;
 import nextstep.member.MemberRequest;
 import nextstep.theme.ThemeRequest;
