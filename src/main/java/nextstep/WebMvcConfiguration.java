f81238bc0cf609667e10c28106ff9b6fe081c860
kyne.k <dd2307@snu.ac.kr>
2023-01-26 14:18:41 +0900

    refactor : auth 패키지 내 Component Scan으로 등록하던 객체를 Java Configuration을 이용하여 등록

--- src/main/java/nextstep/WebMvcConfiguration.java
+++ src/main/java/nextstep/WebMvcConfiguration.java
@@ -2,31 +2,53 @@ package nextstep;
 
 import auth.AdminInterceptor;
 import auth.JwtTokenProvider;
+import auth.LoginController;
 import auth.LoginMemberArgumentResolver;
 import auth.LoginService;
+import nextstep.member.Member;
+import nextstep.member.MemberDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 public class WebMvcConfiguration implements WebMvcConfigurer {
-    private LoginService loginService;
-    private JwtTokenProvider jwtTokenProvider;
 
-    public WebMvcConfiguration(LoginService loginService, JwtTokenProvider jwtTokenProvider) {
-        this.loginService = loginService;
-        this.jwtTokenProvider = jwtTokenProvider;
+    private final MemberDao memberDao;
+
+    @Autowired
+    public WebMvcConfiguration(MemberDao memberDao) {
+        this.memberDao = memberDao;
+    }
+
+    @Bean
+    public JwtTokenProvider jwtTokenProvider() {
+        return new JwtTokenProvider();
+    }
+
+    @Bean
+    public LoginService loginService() {
+        return new LoginService(memberDao, jwtTokenProvider());
+    }
+
+    @Bean
+    public LoginController loginController() {
+        return new LoginController(loginService());
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new AdminInterceptor(jwtTokenProvider)).addPathPatterns("/admin/**");
+        registry.addInterceptor(new AdminInterceptor(jwtTokenProvider())).addPathPatterns("/admin/**");
     }
 
     @Override
     public void addArgumentResolvers(List argumentResolvers) {
-        argumentResolvers.add(new LoginMemberArgumentResolver(loginService));
+        argumentResolvers.add(new LoginMemberArgumentResolver(loginService()));
     }
 }
