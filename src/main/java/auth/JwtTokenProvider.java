cab4f1e3642e31398bd8204ab16fc806ac716e75
jerrie.choi <pinkytime96@gmail.com>
2023-01-26 16:12:22 +0900

    feat: JwtTokenProvider 생성자 추가

--- src/main/java/auth/JwtTokenProvider.java
+++ src/main/java/auth/JwtTokenProvider.java
@@ -1,17 +1,18 @@
 package auth;
 
 import io.jsonwebtoken.*;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
-@Component
 public class JwtTokenProvider {
-    @Value("${security.jwt.token.secret-key}")
-    private String secretKey;
-    @Value("${security.jwt.token.expire-length}")
-    private long validityInMilliseconds;
+
+    private final String secretKey;
+    private final long validityInMilliseconds;
+
+    public JwtTokenProvider(String secretKey, long validityInMilliseconds) {
+        this.secretKey = secretKey;
+        this.validityInMilliseconds = validityInMilliseconds;
+    }
 
     public String createToken(String principal, String role) {
         Claims claims = Jwts.claims().setSubject(principal);
