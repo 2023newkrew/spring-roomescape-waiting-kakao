16a19161e8458cfa2a0fa3f9df02c48bdaf2e161
kyne.k <dd2307@snu.ac.kr>
2023-01-26 17:21:04 +0900

    feat : waiting 생성 및 삭제 구현

--- src/main/java/nextstep/waiting/Waiting.java
+++ src/main/java/nextstep/waiting/Waiting.java
@@ -1,12 +1,41 @@
 package nextstep.waiting;
 
-import lombok.Getter;
+import java.util.Objects;
 import nextstep.member.Member;
 import nextstep.schedule.Schedule;
 
-@Getter
 public class Waiting {
     private Long id;
     private Schedule schedule;
     private Member member;
+
+    public Waiting() {
+    }
+
+    public Waiting(Schedule schedule, Member member) {
+        this.schedule = schedule;
+        this.member = member;
+    }
+
+    public Waiting(Long id, Schedule schedule, Member member) {
+        this.id = id;
+        this.schedule = schedule;
+        this.member = member;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Schedule getSchedule() {
+        return schedule;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public boolean sameMember(Member member) {
+        return member != null && Objects.equals(this.member.getId(), member.getId());
+    }
 }
--- src/main/java/nextstep/waiting/WaitingController.java
+++ src/main/java/nextstep/waiting/WaitingController.java
@@ -1,36 +1,32 @@
 package nextstep.waiting;
 
-import auth.AuthenticationException;
 import auth.LoginMember;
 import auth.UserDetails;
 import java.net.URI;
-import java.util.List;
+import lombok.RequiredArgsConstructor;
 import nextstep.member.Member;
-import org.springframework.http.HttpStatus;
+import nextstep.member.MemberService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class WaitingController {
 
     public final WaitingService waitingService;
+    public final MemberService memberService;
 
-    public WaitingController(WaitingService waitingService) {
-        this.waitingService = waitingService;
+    @PostMapping("/reservation-waitings")
+    public ResponseEntity createWaiting(@LoginMember UserDetails userDetails, @RequestBody WaitingRequest waitingRequest) {
+        Member member = memberService.findById(userDetails.getId());
+        Long id = waitingService.create(member, waitingRequest);
+        
+        return ResponseEntity.created(URI.create("/reservation-waitings/" + id)).build();
     }
-
-//    @PostMapping("/reservation-waitings")
-//    public ResponseEntity createReservation(@LoginMember UserDetails userDetails, @RequestBody int scheduleId) {
-//        Long id = waitingService.create(member, waitingRequest);
-//        return ResponseEntity.created(URI.create("/reservations/" + id)).build();
-//    }
 
 //    @GetMapping("/reservations")
 //    public ResponseEntity readReservations(@RequestParam Long themeId, @RequestParam String date) {
@@ -38,12 +34,13 @@ public class WaitingController {
 //        return ResponseEntity.ok().body(results);
 //    }
 //
-//    @DeleteMapping("/reservations/{id}")
-//    public ResponseEntity deleteReservation(@LoginMember Member member, @PathVariable Long id) {
-//        waitingService.deleteById(member, id);
-//
-//        return ResponseEntity.noContent().build();
-//    }
+    @DeleteMapping("/reservation-waitings/{id}")
+    public ResponseEntity deleteReservation(@LoginMember UserDetails userDetails, @PathVariable Long id) {
+        Member member = memberService.findById(userDetails.getId());
+        waitingService.deleteById(member, id);
+
+        return ResponseEntity.noContent().build();
+    }
 //
 //    @ExceptionHandler(Exception.class)
 //    public ResponseEntity onException(Exception e) {
