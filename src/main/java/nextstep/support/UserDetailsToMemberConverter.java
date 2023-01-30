51c1e58b012e7c437d96237b0eafd8f71666e2d5
엄태호 <qqsscc7342@gmail.com>
2023-01-30 14:07:25 +0900

    refactor: userDetailConverter 구현

---
+++ src/main/java/nextstep/support/UserDetailsToMemberConverter.java
@@ -0,0 +1,21 @@
+package nextstep.support;
+
+import auth.domain.persist.UserDetails;
+import nextstep.domain.persist.Member;
+import org.springframework.core.convert.converter.Converter;
+
+@Deprecated
+public class UserDetailsToMemberConverter implements Converter<UserDetails, Member> {
+
+    @Override
+    public Member convert(UserDetails userDetails) {
+        return new Member(
+            userDetails.getId(),
+            userDetails.getUsername(),
+            userDetails.getPassword(),
+            userDetails.getName(),
+            userDetails.getPhone(),
+            userDetails.getRole()
+        );
+    }
+}
