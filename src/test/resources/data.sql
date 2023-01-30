60eea9a38000d8d60ec87593d958c9b8fbd1aed9
kane.woz <kane.woz@kakaocorp.com>
2023-01-30 18:36:57 +0900

    refactor: sql 파일 중 테스트 전용 데이터셋을 분리

---
+++ src/test/resources/data.sql
@@ -0,0 +1,20 @@
+insert into member (id, name, phone, username, password, is_admin)
+values (1, '어드민', '010-1234-5678', 'admin', '1q2w3e4r!', true),
+       (2, '일반유저0', '010-0000-0000', 'user0', '1q2w3e4r!', false),
+       (3, '일반유저1', '010-0000-0001', 'user1', '1q2w3e4r!', false);
+
+insert into theme(id, name, desc, price)
+values (1, '기본테마', '테마설명', 1234000),
+       (2, '삭제확인테마', '테마설명', 40000);
+
+insert into reservation(id, date, time, name, theme_id, member_id)
+values (1, '1970-01-01', '12:00', '예약예약', 1, 2);
+--
+alter table member
+    alter column id restart with 10;
+alter table theme
+    alter column id restart with 10;
+alter table reservation
+    alter column id restart with 10;
+alter table waiting
+    alter column id restart with 10;
