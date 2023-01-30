8743b25e98b4deff48f951b4252d21a7349fe8c1
yooj929 <jang0929@naver.com>
2023-01-30 15:59:11 +0900

    refactor: 사용하지 않는 메소드 제거 및 context 테스트 추가"

---
+++ src/test/java/nextstep/RoomEscapeApplicationTest.java
@@ -0,0 +1,15 @@
+package nextstep;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomEscapeApplicationTest {
+
+    @Test
+    void contextLoadTest(){
+    }
+
+}
