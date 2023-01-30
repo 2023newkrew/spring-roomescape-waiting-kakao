79ceb7224b21c331be66d5b2b93d927ab3e64e5d
kokomo0 <50624525+kokomo0@users.noreply.github.com>
2023-01-30 18:25:26 +0900

    feat: 예약 대기 기능 구현

---
+++ src/main/java/roomescape/nextstep/reservation/ReservationWaitingService.java
@@ -0,0 +1,45 @@
+package roomescape.nextstep.reservation;
+
+import roomescape.auth.AuthenticationException;
+import roomescape.nextstep.member.Member;
+import roomescape.nextstep.member.MemberDao;
+import roomescape.nextstep.schedule.Schedule;
+import roomescape.nextstep.schedule.ScheduleDao;
+import roomescape.nextstep.support.DuplicateEntityException;
+import roomescape.nextstep.theme.Theme;
+import roomescape.nextstep.theme.ThemeDao;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ReservationWaitingService {
+    public final ReservationDao reservationDao;
+    public final ThemeDao themeDao;
+    public final ScheduleDao scheduleDao;
+    public final MemberDao memberDao;
+
+    public ReservationWaitingService(ReservationDao reservationDao, ThemeDao themeDao, ScheduleDao scheduleDao, MemberDao memberDao) {
+        this.reservationDao = reservationDao;
+        this.themeDao = themeDao;
+        this.scheduleDao = scheduleDao;
+        this.memberDao = memberDao;
+    }
+
+    public Long create(String username, ReservationWaitingRequest reservationWaitingRequest) {
+        Member member = memberDao.findByUsername(username);
+        if (member == null) {
+            throw new AuthenticationException();
+        }
+        Schedule schedule = scheduleDao.findById(reservationWaitingRequest.getScheduleId());
+        if (schedule == null) {
+            throw new NullPointerException();
+        }
+
+        Reservation newReservation = new Reservation(
+                schedule,
+                member
+        );
+        return reservationDao.save(newReservation);
+    }
+}
