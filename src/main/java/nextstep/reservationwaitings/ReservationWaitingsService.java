86bfb068c117ce63963fc1bc440de55f2cd83ab8
jerrie.choi <pinkytime96@gmail.com>
2023-01-30 17:14:53 +0900

    feat: 예약 대기가 없을 때 예약 생성 비즈니스 로직 추가

--- src/main/java/nextstep/reservationwaitings/ReservationWaitingsService.java
+++ src/main/java/nextstep/reservationwaitings/ReservationWaitingsService.java
@@ -1,6 +1,7 @@
 package nextstep.reservationwaitings;
 
 import nextstep.member.Member;
+import nextstep.reservation.Reservation;
 import nextstep.reservation.ReservationDao;
 import nextstep.schedule.Schedule;
 import nextstep.schedule.ScheduleDao;
@@ -13,17 +14,25 @@ public class ReservationWaitingsService {
 
     private final ReservationWaitingsDao reservationWaitingsDao;
     private final ScheduleDao scheduleDao;
+    private final ReservationDao reservationDao;
 
-    public ReservationWaitingsService(ReservationWaitingsDao reservationWaitingsDao, ScheduleDao scheduleDao) {
+    public ReservationWaitingsService(ReservationWaitingsDao reservationWaitingsDao, ScheduleDao scheduleDao, ReservationDao reservationDao) {
         this.reservationWaitingsDao = reservationWaitingsDao;
         this.scheduleDao = scheduleDao;
+        this.reservationDao = reservationDao;
     }
 
-    public long create(Member member, ReservationWaitingRequest request) {
+    public ReservationResult create(Member member, ReservationWaitingRequest request) {
         Schedule schedule = scheduleDao.findById(request.getScheduleId());
         long waitNum = reservationWaitingsDao.findWaitNumByScheduleId(schedule.getId());
+        if (waitNum == 0) {
+            Reservation reservation = new Reservation(schedule, member);
+            long id = reservationDao.save(reservation);
+            return ReservationResult.createReservation(id);
+        }
         ReservationWaitings reservationWaitings = new ReservationWaitings(member, schedule, waitNum + 1);
-        return reservationWaitingsDao.create(reservationWaitings);
+        long id = reservationWaitingsDao.create(reservationWaitings);
+        return ReservationResult.createReservationWaiting(id);
     }
 
     public List<ReservationWaitings> findMyReservationWaitings(Member member) {
