2b0ea26589eeb386bcad52a7e07ae3dfe4edd397
soony.kwonn <soony.kwonn@kakaocorp.com>
2023-01-30 18:02:59 +0900

    feat: custom exception 추가

---
+++ src/main/java/nextstep/support/NoMemberException.java
@@ -0,0 +1,14 @@
+package nextstep.support;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
+public class NoMemberException extends RuntimeException {
+    public NoMemberException() {
+    }
+
+    public NoMemberException(String message) {
+        super(message);
+    }
+}
