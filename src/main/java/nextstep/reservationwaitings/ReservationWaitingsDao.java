910e152f4133083ae255176b0403fa6120695cfc
jerrie.choi <pinkytime96@gmail.com>
2023-01-30 16:34:46 +0900

    feat: 예약 대기 목록 조회 쿼리 추가

--- src/main/java/nextstep/reservationwaitings/ReservationWaitingsDao.java
+++ src/main/java/nextstep/reservationwaitings/ReservationWaitingsDao.java
@@ -1,14 +1,44 @@
 package nextstep.reservationwaitings;
 
+import nextstep.member.Member;
+import nextstep.schedule.Schedule;
+import nextstep.theme.Theme;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import java.sql.PreparedStatement;
+import java.util.List;
 
 @Repository
 public class ReservationWaitingsDao {
+
+    private final RowMapper<ReservationWaitings> rowMapper = (resultSet, rowNum) -> new ReservationWaitings(
+            resultSet.getLong("reservation_waitings.id"),
+            new Member(
+                    resultSet.getLong("member.id"),
+                    resultSet.getString("member.username"),
+                    resultSet.getString("member.password"),
+                    resultSet.getString("member.name"),
+                    resultSet.getString("member.phone"),
+                    resultSet.getString("member.role")
+            ),
+            new Schedule(
+                    resultSet.getLong("schedule.id"),
+                    new Theme(
+                            resultSet.getLong("theme.id"),
+                            resultSet.getString("theme.name"),
+                            resultSet.getString("theme.desc"),
+                            resultSet.getInt("theme.price")
+                    ),
+                    resultSet.getDate("schedule.date").toLocalDate(),
+                    resultSet.getTime("schedule.time").toLocalTime()
+            ),
+
+            resultSet.getLong("reservation_waitings.wait_num")
+    );
 
     private final JdbcTemplate jdbcTemplate;
 
@@ -35,5 +65,19 @@ public class ReservationWaitingsDao {
         }, keyHolder);
 
         return keyHolder.getKey().longValue();
+    }
+
+    public List<ReservationWaitings> findMyReservationWaitings(Long id) {
+        String sql = "SELECT " +
+                "reservation_waitings.id , reservation_waitings.wait_num, " +
+                "schedule.id, schedule.theme_id, schedule.date, schedule.time, " +
+                "theme.id, theme.name, theme.desc, theme.price, " +
+                "member.id, member.username, member.password, member.name, member.phone, member.role " +
+                "FROM reservation_waitings " +
+                "inner join schedule on reservation_waitings.schedule_id = schedule.id " +
+                "inner join member on reservation_waitings.member_id = member.id " +
+                "inner join theme on schedule.theme_id = theme.id " +
+                "WHERE reservation_waitings.member_id = ?";
+        return jdbcTemplate.query(sql, rowMapper, id);
     }
 }
