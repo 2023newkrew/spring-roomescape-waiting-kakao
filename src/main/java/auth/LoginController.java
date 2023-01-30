e642c4ae51d792915484031c821012eac303795b
Seongsu Park <86999506+HolywaterPark@users.noreply.github.com>
2023-01-30 14:05:02 +0900

    refac: Java Configuration으로 빈 등록하도록 변경

--- src/main/java/auth/LoginController.java
+++ src/main/java/auth/LoginController.java
@@ -1,11 +1,11 @@
-package nextstep.auth;
+package auth;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@ResponseBody
 public class LoginController {
     private LoginService loginService;
 
