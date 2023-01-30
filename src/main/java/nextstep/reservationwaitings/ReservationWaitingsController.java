9f02395c8141c2a0a5edce0a2cb07b78ad19c391
jerrie.choi <pinkytime96@gmail.com>
2023-01-30 16:34:35 +0900

    feat: 예약 대기 목록 조회 기능 추가

--- src/main/java/nextstep/reservationwaitings/ReservationWaitingsController.java
+++ src/main/java/nextstep/reservationwaitings/ReservationWaitingsController.java
@@ -3,12 +3,10 @@ package nextstep.reservationwaitings;
 import auth.LoginMember;
 import nextstep.member.Member;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/reservation-waitings")
@@ -24,5 +22,11 @@ public class ReservationWaitingsController {
     public ResponseEntity<Void> create(@LoginMember Member member, @RequestBody ReservationWaitingRequest request) {
         long id = reservationWaitingsService.create(member, request);
         return ResponseEntity.created(URI.create("/reservation-waitings/" + id)).build();
+    }
+
+    @GetMapping("/mine")
+    public ResponseEntity<List<ReservationWaitings>> findMyReservationWaitings(@LoginMember Member member) {
+        List<ReservationWaitings> reservationWaitings = reservationWaitingsService.findMyReservationWaitings(member);
+        return ResponseEntity.ok(reservationWaitings);
     }
 }
