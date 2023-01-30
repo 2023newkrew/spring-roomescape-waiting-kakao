c97a323eda0459fcbc727534c01383694301cf2b
eddie.kk <eddie.kk@kakaocorp.com>
2023-01-30 15:49:45 +0900

    refactor: 테스트 클래스 메서드명 수정 - Service 테스트임을 명시

--- src/test/java/nextstep/reservationwaiting/ReservationWaitingServiceTest.java
+++ src/test/java/nextstep/reservationwaiting/ReservationWaitingServiceTest.java
@@ -24,7 +24,7 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
-public class ReservationWaitingTest {
+public class ReservationWaitingServiceTest {
 
     @Mock
     private ReservationWaitingDao reservationWaitingDao;
