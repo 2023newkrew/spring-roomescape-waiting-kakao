1304eff0a1612d3ada90e843b870e1b3f94cbd5d
jerrie.choi <pinkytime96@gmail.com>
2023-01-30 18:36:46 +0900

    feat: 예약 대기 삭제 권한 비즈니스 로직 추가

--- src/main/java/nextstep/reservationwaitings/ReservationWaitingsService.java
+++ src/main/java/nextstep/reservationwaitings/ReservationWaitingsService.java
@@ -5,6 +5,7 @@ import nextstep.reservation.Reservation;
 import nextstep.reservation.ReservationDao;
 import nextstep.schedule.Schedule;
 import nextstep.schedule.ScheduleDao;
+import nextstep.support.NotCreatorMemberException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -24,12 +25,13 @@ public class ReservationWaitingsService {
 
     public ReservationResult create(Member member, ReservationWaitingRequest request) {
         Schedule schedule = scheduleDao.findById(request.getScheduleId());
-        long waitNum = reservationWaitingsDao.findWaitNumByScheduleId(schedule.getId());
-        if (waitNum == 0) {
+        long reservationSize = reservationDao.findByScheduleId(request.getScheduleId()).size();
+        if (reservationSize == 0) {
             Reservation reservation = new Reservation(schedule, member);
             long id = reservationDao.save(reservation);
             return ReservationResult.createReservation(id);
         }
+        long waitNum = reservationWaitingsDao.findWaitNumByScheduleId(schedule.getId());
         ReservationWaitings reservationWaitings = new ReservationWaitings(member, schedule, waitNum + 1);
         long id = reservationWaitingsDao.create(reservationWaitings);
         return ReservationResult.createReservationWaiting(id);
@@ -37,5 +39,13 @@ public class ReservationWaitingsService {
 
     public List<ReservationWaitings> findMyReservationWaitings(Member member) {
         return reservationWaitingsDao.findMyReservationWaitings(member.getId());
+    }
+
+    public void delete(Member member, Long id) {
+        ReservationWaitings reservationWaitings = reservationWaitingsDao.findById(id);
+        if (reservationWaitings.getMember().getId() != member.getId()) {
+            throw new NotCreatorMemberException();
+        }
+        reservationWaitingsDao.delete(id);
     }
 }
