9f29d2e634b89b2bc2ee2908eb8b70ae4649b8d6
jay.den <jay.den@kakaocorp.com>
2023-01-30 15:12:57 +0900

    feat : 통합테스트 작성
    - [x] 통합테스트 작성

--- src/test/java/nextstep/theme/ThemeE2ETest.java
+++ src/test/java/nextstep/theme/ThemeE2ETest.java
@@ -1,9 +1,9 @@
 package nextstep.theme;
 
-import io.restassured.RestAssured;
-import nextstep.AbstractE2ETest;
 import auth.TokenRequest;
 import auth.TokenResponse;
+import io.restassured.RestAssured;
+import nextstep.AbstractE2ETest;
 import nextstep.member.MemberRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
