89f3bd9546655e6867ebd9a85d506bd1481fefc8
BaeRoNuI <xogh20321@gmail.com>
2023-01-30 16:09:22 +0900

    feat: add globalExceptionHandler

---
+++ src/main/java/nextstep/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package nextstep;
+
+import auth.AuthenticationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity onAuthenticationException(AuthenticationException e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity onException(Exception e) {
+        return ResponseEntity.badRequest().build();
+    }
+}
---
+++ src/main/java/nextstep/support/EntityNotFound.java
@@ -0,0 +1,4 @@
+package nextstep.support;
+
+public class EntityNotFound extends RuntimeException {
+}
