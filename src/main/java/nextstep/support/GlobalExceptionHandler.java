ec7bbecd2de025ca99f34bc803c82d86d29b7dc5
BaeRoNuI <xogh20321@gmail.com>
2023-01-30 18:33:28 +0900

    feat: add PermissionDeniedException

--- src/main/java/nextstep/support/GlobalExceptionHandler.java
+++ src/main/java/nextstep/support/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
-package nextstep;
+package nextstep.support;
 
 import auth.AuthenticationException;
+import nextstep.support.EntityNotFoundException;
+import nextstep.support.PermissionDeniedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,6 +12,16 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
     @ExceptionHandler(AuthenticationException.class)
     public ResponseEntity onAuthenticationException(AuthenticationException e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity onEntityNotFoundException(EntityNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    @ExceptionHandler(PermissionDeniedException.class)
+    public ResponseEntity onPermissionDeniedException(PermissionDeniedException e) {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 
