d4af2f7848c485f8211c9fcd06b7f12f5f27d0a5
엄태호 <qqsscc7342@gmail.com>
2023-01-30 14:07:25 +0900

    refactor: userDetailConverter 구현

--- src/main/java/nextstep/support/UserDetailsToMemberConverter.java
+++ src/main/java/nextstep/support/UserDetailsToMemberConverter.java
@@ -6,6 +6,10 @@ import org.springframework.core.convert.converter.Converter;
 
 @Deprecated
 public class UserDetailsToMemberConverter implements Converter<UserDetails, Member> {
+<<<<<<< HEAD
+=======
+
+>>>>>>> 51c1e58 (refactor: userDetailConverter 구현)
     @Override
     public Member convert(UserDetails userDetails) {
         return new Member(
