e8eca7949b84e0f95f856c86cd049e3ae4b5af0e
kane.woz <kane.woz@kakaocorp.com>
2023-01-31 14:19:08 +0900

    refactor: 설정 클래스 변경과 일부 키, 설정값을 프로퍼티 파일로부터 받아오도록 수정

--- src/main/java/roomescape/config/JWTConfig.java
+++ src/main/java/roomescape/config/JWTConfig.java
@@ -2,16 +2,21 @@ package roomescape.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import roomescape.controller.interceptor.AdminInterceptor;
 import roomescape.controller.interceptor.JWTBearerInterceptor;
+import roomescape.controller.resolver.JWTMemberIdResolver;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
-public class InterceptorConfig implements WebMvcConfigurer {
+public class JWTConfig implements WebMvcConfigurer {
     private final JWTBearerInterceptor jwtBearerInterceptor;
     private final AdminInterceptor adminInterceptor;
+    private final JWTMemberIdResolver jwtMemberIdResolver;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
@@ -19,5 +24,10 @@ public class InterceptorConfig implements WebMvcConfigurer {
                 .order(1);
         registry.addInterceptor(adminInterceptor)
                 .order(2);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(jwtMemberIdResolver);
     }
 }
--- src/main/java/roomescape/service/JWTProvider.java
+++ src/main/java/roomescape/service/JWTProvider.java
@@ -4,6 +4,8 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import roomescape.controller.errors.ErrorCode;
 import roomescape.service.exception.ServiceException;
@@ -12,13 +14,19 @@ import java.util.Date;
 
 @Service
 public class JWTProvider {
-    private static final String SECRET_KEY = "learning-test-spring";
-    private static final long VALIDITY_IN_MILLISECONDS = 3600000;
+
+    @Value("${jwt.secret.symmetric-key}")
+    @Getter
+    private String secretKey;
+
+    @Value("${jwt.secret.validity-in-milliseconds}")
+    @Getter
+    private long validityInMilliseconds;
 
     public Jws<Claims> parse(String jwtToken) {
         try {
             return Jwts.parser()
-                       .setSigningKey(SECRET_KEY)
+                       .setSigningKey(secretKey)
                        .parseClaimsJws(jwtToken);
         } catch (Exception e) {
             throw new ServiceException(ErrorCode.INVALID_TOKEN);
@@ -29,12 +37,12 @@ public class JWTProvider {
         Claims claims = Jwts.claims().setSubject(subject);
         claims.put("is_admin", isAdmin);
         Date now = new Date();
-        Date validity = new Date(now.getTime() + VALIDITY_IN_MILLISECONDS);
+        Date validity = new Date(now.getTime() + validityInMilliseconds);
         return Jwts.builder()
                    .setClaims(claims)
                    .setIssuedAt(now)
                    .setExpiration(validity)
-                   .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                   .signWith(SignatureAlgorithm.HS256, secretKey)
                    .compact();
     }
 }
