d51b07696931f104f08512ed9c502ae498306891
yooj929 <jang0929@naver.com>
2023-01-31 14:02:06 +0900

    refactor: schema comment 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,45 +1,45 @@
 CREATE TABLE reservation
 (
     id          bigint not null auto_increment,
-    schedule_id bigint not null,
-    member_id   bigint not null,
+    schedule_id bigint not null COMMENT 'SCHEDULE PK(FK)',
+    member_id   bigint not null COMMENT 'MEMBER PK(FK)',
     primary key (id)
 );
 
 CREATE TABLE theme
 (
     id    bigint       not null auto_increment,
-    name  varchar(20)  not null,
-    desc  varchar(255) not null,
-    price int          not null,
+    name  varchar(20)  not null COMMENT '테마 명',
+    desc  varchar(255) not null COMMENT '테마 설명',
+    price int          not null COMMENT '1인 테마 이용 가격',
     primary key (id)
 );
 
 CREATE TABLE schedule
 (
     id       bigint not null auto_increment,
-    theme_id bigint not null,
-    date     date   not null,
-    time     time   not null,
+    theme_id bigint not null COMMENT 'THEME PK(FK)',
+    date     date   not null COMMENT '스케쥴이 실행되는 날',
+    time     time   not null COMMENT '스케쥴이 실행되는 시간',
     primary key (id)
 );
 
 CREATE TABLE member
 (
     id       bigint      not null auto_increment,
-    username varchar(20) not null,
-    password varchar(20) not null,
-    name     varchar(20) not null,
-    phone    varchar(20) not null,
-    role     varchar(20) not null,
+    username varchar(20) not null COMMENT '회원의 로그인 ID',
+    password varchar(20) not null COMMENT '회원의 로그인 password',
+    name     varchar(20) not null COMMENT '회원의 이름',
+    phone    varchar(20) not null COMMENT '회원의 Phone 번호 ex) 010-1234-5678',
+    role     varchar(20) not null COMMENT '회원의 역할 ADMIN/USER',
     primary key (id)
 );
 
 CREATE TABLE reservation_waiting
 (
     id          bigint not null auto_increment,
-    schedule_id bigint not null,
-    member_id   bigint not null,
-    wait_num    bigint not null
+    schedule_id bigint not null COMMENT 'SCHEDULE PK(FK)',
+    member_id   bigint not null COMMENT 'MEMBER PK(FK)',
+    wait_num    bigint not null COMMENT '예약 대기 번호'
 );
 
