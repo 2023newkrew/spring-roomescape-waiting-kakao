3d72ce52085a7e3eb2b3c8ddcd9893332110df50
soony.kwonn <soony.kwonn@kakaocorp.com>
2023-02-01 00:42:55 +0900

    refactor: HttpStatus 변경 (UNAUTHORIZED -> FORBIDDEN)

--- src/main/java/auth/AdminInterceptor.java
+++ src/main/java/auth/AdminInterceptor.java
@@ -3,8 +3,6 @@ package auth;
 import auth.support.exception.NotAdminRoleException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,15 +13,14 @@ import java.util.Objects;
 public class AdminInterceptor implements HandlerInterceptor {
     private final JwtTokenProvider jwtTokenProvider;
 
-    // TODO NotAdminRoleException을 던지는 방식으로 변경 필요
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String credential = request.getHeader(HttpHeaders.AUTHORIZATION)
                 .split(" ")[1];
         String role = jwtTokenProvider.getRole(credential);
         if (!Objects.equals(role, "ADMIN")) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "관리자 권한이 없습니다.");
-//            throw new NotAdminRoleException();
+//            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "관리자 권한이 없습니다.");
+            throw new NotAdminRoleException();
         }
         return true;
     }
--- src/main/java/auth/support/exception/NotAdminRoleException.java
+++ src/main/java/auth/support/exception/NotAdminRoleException.java
@@ -3,8 +3,8 @@ package auth.support.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.UNAUTHORIZED)
-public class NotAdminRoleException extends RuntimeException{
+@ResponseStatus(HttpStatus.FORBIDDEN)
+public class NotAdminRoleException extends RuntimeException {
     public NotAdminRoleException() {
     }
 }
