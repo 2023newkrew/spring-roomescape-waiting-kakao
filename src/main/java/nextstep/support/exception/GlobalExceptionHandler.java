0f016b68d8da8784201f90c119297d543aabebf0
soony.kwonn <soony.kwonn@kakaocorp.com>
2023-01-31 14:27:53 +0900

    feat: GlobalExceptionHandler 생성

---
+++ src/main/java/nextstep/support/exception/GlobalExceptionHandler.java
@@ -0,0 +1,40 @@
+package nextstep.support.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(AlreadyReservedScheduleException.class)
+    ResponseEntity<String> handleAlreadyReservedScheduleException(Exception exception) {
+        return ResponseEntity.badRequest()
+                .body(exception.getMessage());
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    ResponseEntity<String> handleAuthenticationException(Exception exception) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(exception.getMessage());
+    }
+
+    @ExceptionHandler(DuplicateEntityException.class)
+    ResponseEntity<String> handleDuplicateEntityException(Exception exception) {
+        return ResponseEntity.badRequest()
+                .body(exception.getMessage());
+    }
+
+    @ExceptionHandler(NoMemberException.class)
+    ResponseEntity<String> handleNoMemberException(Exception exception) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(exception.getMessage());
+    }
+
+    @ExceptionHandler(NoReservationException.class)
+    ResponseEntity<String> handleNoReservationException(Exception exception) {
+        return ResponseEntity.badRequest()
+                .body(exception.getMessage());
+    }
+}
