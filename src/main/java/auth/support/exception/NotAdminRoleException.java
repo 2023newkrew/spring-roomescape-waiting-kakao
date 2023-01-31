954a92a503ad87e4ea1ae1c59344ab51453da2bd
soony.kwonn <soony.kwonn@kakaocorp.com>
2023-02-01 00:36:57 +0900

    feat: ResponseStatus 애노테이션 추가

--- src/main/java/auth/support/exception/NotAdminRoleException.java
+++ src/main/java/auth/support/exception/NotAdminRoleException.java
@@ -1,5 +1,9 @@
 package auth.support.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
 public class NotAdminRoleException extends RuntimeException{
     public NotAdminRoleException() {
     }
