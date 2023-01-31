54525b56630e7256645b33e56e520177b9e26a17
Kloong1 <dkdlsmsm98@gmail.com>
2023-01-31 16:59:02 +0900

    feat: auth 패키지에서 Datasource를 직접 만들지 않고 주입받게 변경

--- src/main/java/auth/AuthConfig.java
+++ src/main/java/auth/AuthConfig.java
@@ -1,26 +1,17 @@
 package auth;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-import javax.sql.DataSource;
-
 @Configuration
-@PropertySource("classpath:db.properties")
 public class AuthConfig {
 
-    @Value("${db.driver}")
-    private String dbDriver;
-    @Value("${db.url}")
-    private String dbUrl;
-    @Value("${db.username}")
-    private String dbUsername;
-    @Value("${db.password}")
-    private String dbPassword;
+    private final JdbcTemplate jdbcTemplate;
+
+    public AuthConfig(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @Bean
     public LoginController loginController() {
@@ -39,21 +30,6 @@ public class AuthConfig {
 
     @Bean
     public MemberDetailsService memberDetailsService() {
-        return new MemberDetailsService(jdbcTemplate());
-    }
-
-    @Bean
-    public JdbcTemplate jdbcTemplate() {
-        return new JdbcTemplate(dataSource());
-    }
-
-    @Bean
-    public DataSource dataSource() {
-        DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName(dbDriver);
-        dataSourceBuilder.url(dbUrl);
-        dataSourceBuilder.username(dbUsername);
-        dataSourceBuilder.password(dbPassword);
-        return dataSourceBuilder.build();
+        return new MemberDetailsService(jdbcTemplate);
     }
 }
