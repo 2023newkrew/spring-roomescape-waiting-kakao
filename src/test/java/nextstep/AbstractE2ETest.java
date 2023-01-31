7741c7b9f9fdb18cc078c07640f2c81c55176311
joelonsw <joel610naver@gmail.com>
2023-01-31 11:49:18 +0900

    test: 테스트 픽스쳐 도입

--- src/test/java/nextstep/AbstractE2ETest.java
+++ src/test/java/nextstep/AbstractE2ETest.java
@@ -10,6 +10,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 
+import static nextstep.auth.AuthE2ETest.createToken;
+import static nextstep.member.MemberE2ETest.createMember;
+
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class AbstractE2ETest {
@@ -21,24 +24,9 @@ public class AbstractE2ETest {
     @BeforeEach
     protected void setUp() {
         MemberRequest memberBody = new MemberRequest(USERNAME, PASSWORD, "name", "010-1234-5678", "ADMIN");
-        RestAssured
-                .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(memberBody)
-                .when().post("/members")
-                .then().log().all()
-                .statusCode(HttpStatus.CREATED.value());
+        createMember(memberBody);
 
         TokenRequest tokenBody = new TokenRequest(USERNAME, PASSWORD);
-        var response = RestAssured
-                .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(tokenBody)
-                .when().post("/login/token")
-                .then().log().all()
-                .statusCode(HttpStatus.OK.value())
-                .extract();
-
-        token = response.as(TokenResponse.class);
+        token = createToken(tokenBody);
     }
 }
--- src/test/java/nextstep/member/MemberE2ETest.java
+++ src/test/java/nextstep/member/MemberE2ETest.java
@@ -11,6 +11,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 
+import java.util.UUID;
+
+import static nextstep.auth.AuthE2ETest.createToken;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
@@ -71,5 +74,35 @@ public class MemberE2ETest {
 
         Member member = response.as(Member.class);
         assertThat(member.getUsername()).isNotNull();
+    }
+
+    public static void createMember(MemberRequest memberRequest) {
+        RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(memberRequest)
+                .when().post("/members")
+                .then().log().all()
+                .statusCode(HttpStatus.CREATED.value());
+    }
+
+    public static TokenResponse createAdminMemberAndIssueToken() {
+        String username = UUID.randomUUID().toString().substring(0, 10);
+        String password = UUID.randomUUID().toString().substring(0, 10);
+        MemberRequest memberRequest = new MemberRequest(username, password, "name", "010-1234-5678", "ADMIN");
+        return createMemberAndIssueToken(memberRequest);
+    }
+
+    public static TokenResponse createNormalMemberAndIssueToken() {
+        String username = UUID.randomUUID().toString().substring(0, 10);
+        String password = UUID.randomUUID().toString().substring(0, 10);
+        MemberRequest memberRequest = new MemberRequest(username, password, "name", "010-1234-5678", "MEMBER");
+        return createMemberAndIssueToken(memberRequest);
+    }
+
+    private static TokenResponse createMemberAndIssueToken(MemberRequest memberRequest) {
+        createMember(memberRequest);
+        TokenRequest tokenRequest = new TokenRequest(memberRequest.getUsername(), memberRequest.getPassword());
+        return createToken(tokenRequest);
     }
 }
