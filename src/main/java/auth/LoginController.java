8c983ac196d6a58a39f60dfd178ea1f246fff505
herbi.sf8 <herbi.sf8@kakaocorp.com>
2023-01-31 09:10:10 +0900

    refactor: LoginController RestController 어노테이션 제거, RequestMapping 어노테이션 추가
    - java configuration으로 bean 등록하기 떄문에 Controller 어노테이션 제거

--- src/main/java/auth/LoginController.java
+++ src/main/java/auth/LoginController.java
@@ -4,9 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@RequestMapping
 @RequiredArgsConstructor
 public class LoginController {
     private final LoginService loginService;
