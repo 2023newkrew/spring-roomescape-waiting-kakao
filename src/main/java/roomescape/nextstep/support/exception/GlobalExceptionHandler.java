8fbb0959ce9e38648f74fa52ce985b6b2707316e
jungin0507 <a01033046758@gmail.com>
2023-02-01 17:34:44 +0900

    feat: Bean Validation 예외 핸들러 구현

--- src/main/java/roomescape/nextstep/support/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/nextstep/support/exception/GlobalExceptionHandler.java
@@ -1,10 +1,18 @@
 package roomescape.nextstep.support.exception;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import roomescape.auth.AuthenticationException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -23,5 +31,25 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(status)
                 .body(new ErrorResponse(status.value(), "잘못된 인증 정보 입니다."));
+    }
+
+    @SneakyThrows(JsonProcessingException.class)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException e) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, String> errorInfos = new HashMap<>();
+
+        e.getBindingResult()
+                .getAllErrors()
+                .forEach((error) -> {
+                    String fieldName = ((FieldError) error).getField();
+                    String errorMessage = error.getDefaultMessage();
+                    errorInfos.put(fieldName, errorMessage);
+                });
+
+        return ResponseEntity
+                .status(status)
+                .body(new ErrorResponse(status.value(), objectMapper.writeValueAsString(errorInfos)));
     }
 }
