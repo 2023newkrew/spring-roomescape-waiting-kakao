bed6ec768a9b3efb096f480e38070f9167cecf21
jungin0507 <a01033046758@gmail.com>
2023-02-01 17:26:16 +0900

    feat: AuthenticationException 핸들링 메소드 구현

--- src/main/java/roomescape/nextstep/support/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/nextstep/support/exception/GlobalExceptionHandler.java
@@ -1,8 +1,10 @@
 package roomescape.nextstep.support.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import roomescape.auth.AuthenticationException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -13,5 +15,13 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(errorCode.getHttpStatus())
                 .body(new ErrorResponse(errorCode));
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ErrorResponse> handleAuthenticationException(AuthenticationException e) {
+        HttpStatus status = HttpStatus.UNAUTHORIZED;
+        return ResponseEntity
+                .status(status)
+                .body(new ErrorResponse(status.value(), "잘못된 인증 정보 입니다."));
     }
 }
