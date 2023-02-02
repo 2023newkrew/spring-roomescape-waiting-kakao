f15525e9534c94943f50b01fbcc72a5c1ce50b6b
herbi.sf8 <herbi.sf8@kakaocorp.com>
2023-02-02 09:21:50 +0900

    refactor: TokenExtractor 불필요한 변수 제거

--- src/main/java/auth/jwt/TokenExtractor.java
+++ src/main/java/auth/jwt/TokenExtractor.java
@@ -4,12 +4,10 @@ import java.util.Optional;
 
 public class TokenExtractor {
     public Optional<String> extractToken(String tokenWithClass) {
-        String token;
         try {
-            token = tokenWithClass.split(" ")[1];
+            return Optional.of(tokenWithClass.split(" ")[1]);
         } catch (NullPointerException | IndexOutOfBoundsException e) {
             return Optional.empty();
         }
-        return Optional.ofNullable(token);
     }
 }
