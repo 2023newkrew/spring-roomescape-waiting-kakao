002c124ea25623e485b2abc7928d7d4f2eaadb04
herbi.sf8 <herbi.sf8@kakaocorp.com>
2023-02-02 18:07:03 +0900

    refactor: userValidator -> userAuthenticator 변수 이름 변경

--- src/main/java/auth/service/LoginService.java
+++ src/main/java/auth/service/LoginService.java
@@ -16,11 +16,11 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class LoginService {
     private final JwtTokenProvider jwtTokenProvider;
-    private final UserAuthenticator userValidator;
+    private final UserAuthenticator userAuthenticator;
     private final TokenExtractor tokenExtractor;
 
     public TokenResponse createToken(TokenRequest tokenRequest) {
-        UserDetails userDetails = userValidator.authenticate(tokenRequest.getUsername(), tokenRequest.getPassword())
+        UserDetails userDetails = userAuthenticator.authenticate(tokenRequest.getUsername(), tokenRequest.getPassword())
                 .orElseThrow(() -> new AuthenticationException(AuthExceptionCode.AUTHENTICATION_FAIL));
         String accessToken = jwtTokenProvider.createToken(userDetails.getId().toString());
 
@@ -43,7 +43,7 @@ public class LoginService {
 
     public boolean isAdmin(String authorizationHeader) {
         Optional<Long> memberId = extractMemberId(authorizationHeader);
-        return memberId.filter(userValidator::isAdmin).isPresent();
+        return memberId.filter(userAuthenticator::isAdmin).isPresent();
     }
 
 }
