f247d0db8c1b5fb60848eb5cf9804883e0c2e3a0
Kloong1 <dkdlsmsm98@gmail.com>
2023-02-02 18:27:49 +0900

    test: ReservationWaitingE2ETest 재작성

--- src/test/java/nextstep/waiting/ReservationWaitingE2ETest.java
+++ src/test/java/nextstep/waiting/ReservationWaitingE2ETest.java
@@ -1,19 +1,18 @@
 package nextstep.waiting;
 
-import auth.dto.TokenRequest;
-import auth.dto.TokenResponse;
+import auth.token.JwtTokenProvider;
 import io.restassured.RestAssured;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
-import nextstep.AbstractE2ETest;
-import nextstep.member.MemberRequest;
-import nextstep.reservation.ReservationRequest;
+import nextstep.AbstractTest;
+import nextstep.error.ErrorCode;
+import nextstep.member.Member;
 import nextstep.role.Role;
-import nextstep.schedule.ScheduleRequest;
-import nextstep.theme.ThemeRequest;
+import nextstep.schedule.Schedule;
+import nextstep.theme.Theme;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -22,214 +21,221 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
 
-public class ReservationWaitingE2ETest extends AbstractE2ETest {
-    public static final String DATE = "2022-08-11";
-    public static final String TIME = "13:00";
+class ReservationWaitingE2ETest extends AbstractTest {
 
-    private ReservationRequest request;
-    private Long themeId;
-    private Long scheduleId;
+    @Autowired
+    JwtTokenProvider jwtTokenProvider;
 
-    public static final String USERNAME2 = "username2";
-    public static final String PASSWORD2 = "password2";
-
-    public static final String USERNAME3 = "username3";
-    public static final String PASSWORD3 = "password3";
-
-    protected TokenResponse token2;
-    protected TokenResponse token3;
+    Theme theme;
+    Schedule schedule;
 
     @BeforeEach
-    public void setUp() {
-        super.setUp();
-
-        MemberRequest memberBody = new MemberRequest(USERNAME2, PASSWORD2, "name", "010-1234-5678", Role.ADMIN.name());
-        RestAssured
-                .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(memberBody)
-                .when().post("/members")
-                .then().log().all()
-                .statusCode(HttpStatus.CREATED.value());
-
-        TokenRequest tokenBody = new TokenRequest(USERNAME2, PASSWORD2);
-        var response = RestAssured
-                .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(tokenBody)
-                .when().post("/login/token")
-                .then().log().all()
-                .statusCode(HttpStatus.OK.value())
-                .extract();
-
-        token2 = response.as(TokenResponse.class);
-        //////////
-        MemberRequest memberBody2 = new MemberRequest(USERNAME3, PASSWORD3, "name", "010-1234-5678", Role.ADMIN.name());
-        RestAssured
-                .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(memberBody2)
-                .when().post("/members")
-                .then().log().all()
-                .statusCode(HttpStatus.CREATED.value());
-
-        TokenRequest tokenBody2 = new TokenRequest(USERNAME3, PASSWORD3);
-        var response2 = RestAssured
-                .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(tokenBody2)
-                .when().post("/login/token")
-                .then().log().all()
-                .statusCode(HttpStatus.OK.value())
-                .extract();
-
-        token3 = response2.as(TokenResponse.class);
-
-        //////////
-        ThemeRequest themeRequest = new ThemeRequest("테마이름", "테마설명", 22000);
-        var themeResponse = RestAssured
-                .given().log().all()
-                .auth().oauth2(token.getAccessToken())
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(themeRequest)
-                .when().post("/admin/themes")
-                .then().log().all()
-                .statusCode(HttpStatus.CREATED.value())
-                .extract();
-        String[] themeLocation = themeResponse.header("Location").split("/");
-        themeId = Long.parseLong(themeLocation[themeLocation.length - 1]);
-
-        ScheduleRequest scheduleRequest = new ScheduleRequest(themeId, DATE, TIME);
-        var scheduleResponse = RestAssured
-                .given().log().all()
-                .auth().oauth2(token.getAccessToken())
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(scheduleRequest)
-                .when().post("/admin/schedules")
-                .then().log().all()
-                .statusCode(HttpStatus.CREATED.value())
-                .extract();
-        String[] scheduleLocation = scheduleResponse.header("Location").split("/");
-        scheduleId = Long.parseLong(scheduleLocation[scheduleLocation.length - 1]);
-
-        request = new ReservationRequest(
-                scheduleId
-        );
+    void setUp() {
+        theme = createNewTheme();
+        schedule = createNewSchedule(theme.getId());
     }
 
-    @DisplayName("예약이 이미 존재하는 경우 예약 대기를 생성한다")
-    @Test
-    void createWaiting() {
-        createReservation();
+    @Nested
+    @DisplayName("예약 대기 생성 요청 api는")
+    class CreateReservationWaiting {
+        @Nested
+        @DisplayName("예약 대기 생성 요청을 받은 스케줄에 예약이 존재하면")
+        class hasReservation {
+            String token;
 
-        RestAssured
-                .given().log().all()
-                .auth().oauth2(token2.getAccessToken())
-                .body(request)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().post("/reservation-waitings")
-                .then().log().all()
-                .statusCode(HttpStatus.CREATED.value())
-                .header(HttpHeaders.LOCATION, containsString("/reservation-waitings/"));
+            @BeforeEach
+            void setUp() {
+                Member member1 = createNewMember(Role.USER);
+                createNewReservation(schedule.getId(), member1);
+
+                Member member2 = createNewMember(Role.USER);
+                token = jwtTokenProvider.createToken(String.valueOf(member2.getId()), member2.getUsername(), member2.getRole());
+            }
+
+            @Test
+            @DisplayName("201 코드와 함께 /reservation-waitings/ 가 포함된 Location 헤더값을 응답으로 보낸다")
+            void responseReservationWaiting() {
+                ReservationWaitingRequest reservationWaitingRequest = new ReservationWaitingRequest(schedule.getId());
+
+                RestAssured
+                        .given().log().all()
+                        .auth().oauth2(token)
+                        .body(reservationWaitingRequest)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .when().post("/reservation-waitings")
+                        .then().log().all()
+                        .statusCode(HttpStatus.CREATED.value())
+                        .header(HttpHeaders.LOCATION, containsString("/reservation-waitings/"));
+            }
+        }
+
+        @Nested
+        @DisplayName("예약 대기 생성 요청을 받은 스케줄에 예약이 없으면")
+        class noReservation {
+            String token;
+
+            @BeforeEach
+            void setUp() {
+                Member member = createNewMember(Role.USER);
+                token = jwtTokenProvider.createToken(String.valueOf(member.getId()), member.getUsername(), member.getRole());
+            }
+
+            @Test
+            @DisplayName("201 코드와 함께 /reservations/ 가 포함된 Location 헤더값을 응답으로 보낸다")
+            void responseReservationWaiting() {
+                ReservationWaitingRequest reservationWaitingRequest = new ReservationWaitingRequest(schedule.getId());
+
+                RestAssured
+                        .given().log().all()
+                        .auth().oauth2(token)
+                        .body(reservationWaitingRequest)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .when().post("/reservation-waitings")
+                        .then().log().all()
+                        .statusCode(HttpStatus.CREATED.value())
+                        .header(HttpHeaders.LOCATION, containsString("/reservations/"));
+            }
+        }
     }
 
-    @DisplayName("예약이 없는 경우 예약 대기를 생성하면 예약이 된다")
-    @Test
-    void create() {
-        RestAssured
-                .given().log().all()
-                .auth().oauth2(token.getAccessToken())
-                .body(request)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().post("/reservation-waitings")
-                .then().log().all()
-                .statusCode(HttpStatus.CREATED.value())
-                .header(HttpHeaders.LOCATION, containsString("/reservations/"));
+    @Nested
+    @DisplayName("멤버의 모든 예약 대기 조회 api는")
+    class findAllReservationWaitingsOfMember {
+        @Nested
+        @DisplayName("멤버가 여러개의 예약 대기를 가지고 있으면")
+        class hasReservationWaitings {
+            String token;
+
+            ReservationWaiting reservationWaiting1;
+            ReservationWaiting reservationWaiting2;
+            ReservationWaiting reservationWaiting3;
+
+            @BeforeEach
+            void setUp() {
+                Schedule schedule1 = createNewSchedule(theme.getId());
+                Schedule schedule2 = createNewSchedule(theme.getId());
+
+                Member member1 = createNewMember(Role.USER);
+                createNewReservation(schedule.getId(), member1);
+                createNewReservation(schedule1.getId(), member1);
+                createNewReservation(schedule2.getId(), member1);
+
+                Member member2 = createNewMember(Role.USER);
+                reservationWaiting1 = createNewReservationWaiting(schedule.getId(), member2);
+                reservationWaiting2 = createNewReservationWaiting(schedule1.getId(), member2);
+                reservationWaiting3 = createNewReservationWaiting(schedule2.getId(), member2);
+
+                token = jwtTokenProvider.createToken(String.valueOf(member2.getId()), member2.getUsername(), member2.getRole());
+            }
+
+            @Test
+            @DisplayName("200 코드와 함께 메시지 바디에 멤버가 가진 예약 대기 리스트를 담아서 응답한다")
+            void responseReseravtionWaitingLiss() {
+                var response = RestAssured
+                        .given().log().all()
+                        .auth().oauth2(token)
+                        .when().get("/reservation-waitings/mine")
+                        .then().log().all()
+                        .statusCode(HttpStatus.OK.value())
+                        .extract();
+
+                List<ReservationWaiting> reservations = response.jsonPath().getList(".", ReservationWaiting.class);
+                assertThat(reservations)
+                        .hasSize(3)
+                        .contains(reservationWaiting1, reservationWaiting2, reservationWaiting3);
+            }
+        }
     }
 
-    @DisplayName("예약 대기를 조회한다")
-    @Test
-    void showMyWaitings() {
-        createReservation();
-        createReservationWaiting();
+    @Nested
+    @DisplayName("예약 대기 삭제 api는")
+    class DeleteReservationWaiting {
+        @Nested
+        @DisplayName("예약 대기 id에 해당하는 예약 대기가 존재하고, 해당 예약 대기의 소유자가 토큰에 해당하는 멤버이면")
+        class ExistReservationWaitingAndIsOwner {
+            String token;
+            ReservationWaiting reservationWaiting;
 
-        var response = RestAssured
-                .given().log().all()
-                .auth().oauth2(token2.getAccessToken())
-                .when().get("/reservation-waitings/mine")
-                .then().log().all()
-                .extract();
+            @BeforeEach
+            void setUp() {
+                Member member1 = createNewMember(Role.USER);
+                createNewReservation(schedule.getId(), member1);
 
-        List<ReservationWaiting> reservations = response.jsonPath().getList(".", ReservationWaiting.class);
-        assertThat(reservations.size()).isEqualTo(1);
-    }
+                Member member2 = createNewMember(Role.USER);
+                reservationWaiting = createNewReservationWaiting(schedule.getId(), member2);
 
-    @DisplayName("예약 대기를 삭제한다")
-    @Test
-    void delete() {
-        createReservation();
-        var reservationWaiting = createReservationWaiting();
+                token = jwtTokenProvider.createToken(String.valueOf(member2.getId()), member2.getUsername(), member2.getRole());
+            }
 
-        RestAssured
-                .given().log().all()
-                .auth().oauth2(token3.getAccessToken())
-                .body(request)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().post("/reservation-waitings")
-                .then().log().all()
-                .extract();
+            @Test
+            @DisplayName("예약 대기를 삭제하고 204 코드로 응답한다")
+            void responseNoContent() {
+                RestAssured
+                        .given().log().all()
+                        .auth().oauth2(token)
+                        .when().delete("/reservation-waitings/" + reservationWaiting.getReservationId())
+                        .then().log().all()
+                        .statusCode(HttpStatus.NO_CONTENT.value());
+            }
+        }
 
-        RestAssured
-                .given().log().all()
-                .auth().oauth2(token2.getAccessToken())
-                .when().get("/reservation-waitings/mine")
-                .then().log().all()
-                .extract();
+        @Nested
+        @DisplayName("예약 대기 id에 해당하는 예약 대기가 존재하지만 해당 예약 대기의 소유자가 토큰에 해당하는 멤버가 아니면")
+        class ExistReservationWaitingButIsNotOwner {
+            String token;
+            ReservationWaiting reservationWaiting;
 
-        var response = RestAssured
-                .given().log().all()
-                .auth().oauth2(token2.getAccessToken())
-                .when().delete(reservationWaiting.header("Location"))
-                .then().log().all()
-                .extract();
+            @BeforeEach
+            void setUp() {
+                Member member1 = createNewMember(Role.USER);
+                createNewReservation(schedule.getId(), member1);
 
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+                Member member2 = createNewMember(Role.USER);
+                reservationWaiting = createNewReservationWaiting(schedule.getId(), member2);
 
-        RestAssured
-                .given().log().all()
-                .auth().oauth2(token2.getAccessToken())
-                .when().get("/reservation-waitings/mine")
-                .then().log().all()
-                .extract();
+                Member member3 = createNewMember(Role.USER);
+                token = jwtTokenProvider.createToken(String.valueOf(member3.getId()), member3.getUsername(), member3.getRole());
+            }
 
-        RestAssured
-                .given().log().all()
-                .auth().oauth2(token3.getAccessToken())
-                .when().get("/reservation-waitings/mine")
-                .then().log().all()
-                .extract();
-    }
+            @Test
+            @DisplayName("403코드와 함께 ErrorCode.FORBIDDEN 에 해당하는 코드와 설명을 바디에 담아서 응답한다")
+            void responseForbidden() {
+                RestAssured
+                        .given().log().all()
+                        .auth().oauth2(token)
+                        .when().delete("/reservation-waitings/" + reservationWaiting.getReservationId())
+                        .then().log().all()
+                        .statusCode(HttpStatus.FORBIDDEN.value())
+                        .body("code", is(ErrorCode.FORBIDDEN.getCode()))
+                        .body("message", is(ErrorCode.FORBIDDEN.getDescription()));
+            }
+        }
 
-    private ExtractableResponse<Response> createReservation() {
-        return RestAssured
-                .given().log().all()
-                .auth().oauth2(token.getAccessToken())
-                .body(request)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().post("/reservations")
-                .then().log().all()
-                .extract();
-    }
+        @Nested
+        @DisplayName("삭제 요청을 받은 예약 대기가 존재하지 않으면")
+        class NoReservationWaiting {
+            String token;
 
-    private ExtractableResponse<Response> createReservationWaiting() {
-        return RestAssured
-                .given().log().all()
-                .auth().oauth2(token2.getAccessToken())
-                .body(request)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().post("/reservation-waitings")
-                .then().log().all()
-                .extract();
+            @BeforeEach
+            void setUp() {
+                Member member = createNewMember(Role.USER);
+                token = jwtTokenProvider.createToken(String.valueOf(member.getId()), member.getUsername(), member.getRole());
+            }
+
+            @Test
+            @DisplayName("404코드와 함께 ErrorCode.RESERVATION_NOT_FOUND 에 해당하는 코드와 설명을 바디에 담아서 응답한다")
+            void responseNotFound() {
+                RestAssured
+                        .given().log().all()
+                        .auth().oauth2(token)
+                        .when().delete("/reservation-waitings/1")
+                        .then().log().all()
+                        .statusCode(HttpStatus.NOT_FOUND.value())
+                        .body("code", is(ErrorCode.RESERVATION_NOT_FOUND.getCode()))
+                        .body("message", is(ErrorCode.RESERVATION_NOT_FOUND.getDescription()));
+            }
+        }
     }
 }
